36594833978fc340763f1db69e5d47b2dc5e79d1
b-munar <b.munar@uniandes.edu.co>
2024-04-09 22:56:30 -0500

    test: first test, ping

--- src/main.rs
+++ src/main.rs
@@ -36,24 +36,69 @@ async fn main() {
 
     let state = AppState { conn };
 
-    let user_router = Router::new()
-        .route("/ping", get(ping))
-        .nest("/", auth_router());
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
 
-    let app = Router::new().nest("/auth", user_router).with_state(state).layer(
+    axum::serve(listener, app(user_router(), state)).await.unwrap();
+}
+
+pub fn user_router() -> Router<AppState> {
+    Router::new()
+        .route("/ping", get(ping))
+        .nest("/", auth_router())
+}
+
+pub fn app(user_router: Router<AppState>, state:AppState) -> Router {
+    Router::new().nest("/auth", user_router).with_state(state)
+    .layer(
         TraceLayer::new_for_http()
             .make_span_with(trace::DefaultMakeSpan::new()
                 .level(Level::INFO))
             .on_response(trace::DefaultOnResponse::new()
                 .level(Level::INFO)),
 
-    );
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
-
-    axum::serve(listener, app).await.unwrap();
+    )
 }
 
 async fn ping() -> String {
     return "ping".to_string();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+
+    use tower:: ServiceExt;
+    use dotenv::dotenv;
+
+
+
+    #[tokio::test]
+    async fn ping() {
+        dotenv().ok();
+
+
+        let database_url = std::env::var("AUTH_DATABASE_URL_PATH")
+            .unwrap();
+    
+        let conn = Database::connect(database_url)
+            .await
+            .expect("Database connection failed");
+    
+        let state = AppState { conn };
+        let app = app(user_router(), state);
+
+        // `Router` implements `tower::Service<Request<Body>>` so we can
+        // call it like any tower service, no need to run an HTTP server.
+        let response = app
+            .oneshot(Request::builder().uri("/auth/ping").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+    }
 }
